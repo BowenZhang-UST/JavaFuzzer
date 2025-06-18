3251a39a9398ecdf78645bd578c50b5e6c39f47a
BowenZhang-UST <bowen.zhang@connect.ust.hk>
2025-06-18 14:52:02 +0800

    add compile

---
+++ script/compile.sh
@@ -0,0 +1,19 @@
+SUITE_NAME=$1
+if [ -z "$SUITE_NAME" ]; then
+    echo suite name missing.
+    exit
+fi
+
+echo ---$SUITE_NAME--- > err.txt
+for subdir in $SUITE_NAME/*; do
+    echo $subdir
+    ../llvm-12/bin/llvm-dis $subdir/javafuzzer.bc 
+    python3 ../python/linker/jellyfish-link.py ./lib.ll $subdir/javafuzzer.ll -o $subdir/all.ll
+
+    ../llvm-12/bin/clang -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib $subdir/all.ll -o $subdir/a.out 
+
+    timeout 3s $subdir/a.out > $subdir/out.txt
+    if [ $? -ne 0 ]; then
+        echo $subdir >> err.txt
+    fi
+done
